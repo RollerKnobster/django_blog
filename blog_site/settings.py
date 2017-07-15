b60069ccde42f906d2f1de3aa1c75b1bf48d3d63
RollerKnobster <thedoorknobster@gmail.com>
2017-07-15 13:45:51 +0300

    Added post sharing via email and comment model

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -120,3 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'thedoorknobster@gmail.com'
+EMAIL_HOST_PASSWORD = 'StromaeConglomerate97'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
